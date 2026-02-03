efce195954ca8e0281cb8654f8b543f269a1f96f
Ohno17 <ajaylego30@gmail.com>
2026-02-03 02:29:03 -0600

    first commit

---
+++ js/bfworker.js
@@ -0,0 +1,67 @@
+
+function bfworker(array, commands) {
+    this.array = array;
+    this.commands = commands;
+    this.operations = {};
+
+    this.held = 0;
+    this.active = false;
+
+    this.pcstack = [];
+    this.pc = 0;
+    this.dc = 0;
+
+    let self = this;
+
+    this.operations["+"] = function() {
+        if (self.held == 0) return;
+        self.held--;
+        self.array[self.dc]++;
+        if (self.array[self.dc] > 255) self.dc = 0;
+    };
+    this.operations["-"] = function() {
+        if (self.array[self.dc] == 0) return;
+        self.held++;
+        self.array[self.dc]--;
+    };
+    this.operations[">"] = function() {
+        self.dc++;
+        if (self.dc >= self.array.length) self.dc = 0;
+    };
+    this.operations["<"] = function() {
+        self.dc--;
+        if (self.dc < 0) self.dc = self.array.length - 1;
+    };
+    this.operations["."] = function() {
+        console.log(self.array[self.dc]);
+    };
+    this.operations["["] = function() {
+        self.pcstack.push(self.pc);
+    };
+    this.operations["]"] = function() {
+        if (self.array[self.dc] != 0) self.pc = self.pcstack.pop() - 1;
+    };
+    this.operations["}"] = function() {
+        if (self.held != 0) self.pc = self.pcstack.pop() - 1;
+    };
+}
+
+bfworker.prototype.reset = function(commands) {
+    this.commands = commands;
+    this.active = false;
+    this.pcstack = [];
+    this.pc = 0;
+    this.dc = 0;
+}
+
+bfworker.prototype.run = function() {
+    console.log(this.commands[this.pc]);
+    
+    const func = this.operations[this.commands[this.pc]];
+    if (func) func();
+    this.pc++;
+    if (this.pc >= this.commands.length) {
+        this.pc = 0;
+        this.active = false;
+    }
+};
---
+++ js/script.js
@@ -0,0 +1,120 @@
+const display = document.getElementById("display");
+const workers = document.getElementById("workers")
+
+const genspeedcost = document.getElementById("genspeedcost");
+const genamtcost = document.getElementById("genamtcost");
+const workercost = document.getElementById("workercost");
+
+const array = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+
+const gameData = {
+    "value": 0,
+    "genTime": 1000,
+    "genAmount": 1,
+
+    "genspeedcost": 20,
+    "genamtcost": 10,
+    "workercost": 5,
+
+    "workers": [
+        {"code": "++++[->+<].", "held": 0, "active": false}
+    ]
+};
+
+var generatorInterval = -1;
+var workerExecutors = [
+    {"executor": new bfworker(array, gameData.workers[0].code), "held": null, "active": null}
+];
+
+function displayCosts() {
+    genspeedcost.innerHTML = gameData.genspeedcost;
+    genamtcost.innerHTML = gameData.genamtcost;
+    workercost.innerHTML = gameData.workercost;
+}
+
+function displayArray() {
+    display.innerHTML = "";
+    for (let i = 0; i < array.length; i++) {
+        if (i == array.length - 1)
+            display.innerHTML += "<" + array[i] + "> ";
+        else
+            display.innerHTML += array[i] + " ";
+    }
+}
+
+function displayWorkers() {
+    workers.innerHTML = "";
+    for (let i = 0; i < gameData.workers.length; i++) {
+        const row = document.createElement("tr");
+        const tdtext = document.createElement("td");
+        const tdheld = document.createElement("td");
+        const tdactive = document.createElement("td");
+        const textarea = document.createElement("textarea");
+        const checkbox = document.createElement("input");
+
+        textarea.innerHTML = gameData.workers[i].code;
+        textarea.rows = "6";
+        textarea.cols = "40";
+        textarea.onchange = function(event) {
+            workerExecutors[i].executor.reset(textarea.value);
+        };
+        tdheld.innerHTML = gameData.workers[i].held + "v";
+        workerExecutors[i].held = tdheld;
+        checkbox.type = "checkbox";
+        checkbox.checked = gameData.workers[i].active;
+        workerExecutors[i].active = checkbox;
+        checkbox.onchange = function(event) {
+            workerExecutors[i].executor.reset(textarea.value);
+            workerExecutors[i].executor.active = checkbox.checked;
+        };
+
+        tdtext.append(textarea);
+        tdactive.append(checkbox);
+
+        row.append(tdtext);
+        row.append(tdheld);
+        row.append(tdactive);
+
+        workers.append(row);
+    }
+}
+
+function setGeneratorUpdate() {
+    if (generatorInterval !== -1) clearInterval(generatorInterval);
+    generatorInterval = setInterval(generatorUpdate, gameData.genTime);
+}
+
+function generatorUpdate() {
+    const cellIndex = Math.floor((array.length - 1) * Math.random());
+    array[cellIndex] += gameData.genAmount;
+    displayArray();
+}
+
+function sellerUpdate() {
+    if (array[array.length - 1] > 0) array[array.length - 1]--;
+    gameData.value++;
+}
+
+function workersUpdate() {
+    for (let i = 0; i < workerExecutors.length; i++) {
+        gameData.workers[i].code = workerExecutors[i].executor.commands;
+        gameData.workers[i].held = workerExecutors[i].executor.held;
+        gameData.workers[i].active = workerExecutors[i].executor.active;
+        workerExecutors[i].held.innerHTML = workerExecutors[i].executor.held;
+        workerExecutors[i].active.checked = workerExecutors[i].executor.active;
+
+        if (!workerExecutors[i].executor.active) continue;
+
+        setTimeout(function() {
+            workerExecutors[i].executor.run();
+        }, Math.random() * 500);
+    }
+}
+
+displayWorkers();
+displayArray();
+displayCosts();
+
+setGeneratorUpdate();
+setInterval(sellerUpdate, 350);
+setInterval(workersUpdate, 500);
